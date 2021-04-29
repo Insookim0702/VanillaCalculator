4b5e8315cb30967f928fea13f97eda4cc5d86252
insookim0702 <insookim0702@gmail.com>
2021-04-29 21:50:05 +0900

    - 변수 값 타입은 항상 문자열. - 연속으로 연산자를 입력받을 경우 처리.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@
 
     CLEAR.addEventListener('click', () => {
         EXPRESSION.innerText = '';
-        INPUT_VALUE.value = 0;
+        INPUT_VALUE.value = '0';
     });
 
     INPUT_VALUE.addEventListener('keyup', (event) => {
@@ -41,7 +41,7 @@
 
     DELETE.addEventListener('click', () => {
         if (INPUT_VALUE.value.length === 1) {
-            INPUT_VALUE.value = 0;
+            INPUT_VALUE.value = '0';
             return;
         }
         INPUT_VALUE.value = INPUT_VALUE.value.substr(0, INPUT_VALUE.value.length - 1);
@@ -50,21 +50,24 @@
     function checkValid(value) {
         const koreanRex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
         const englishRex = /[a-z|A-Z]/;
-
+        // 숫자만 입력된 경우
+        const onlyNumberRex = /^[0-9]*$/g;
         // 연산자가 연달아 입력된 경우
-
+        const seriesOperator = /[*\/+-]{2,}/;
         // 수식 앞뒤로 연산자가 있는 경우
-        if (/^[+-/*]/.test(value) || /[+-/*]$/.test(value)) {
+        if (value.match(seriesOperator) || value.match(onlyNumberRex) || /^[+-/*]/.test(value) || /[+-/*]$/.test(value)) {
             ALERTBAR.style.display = 'flex';
             ALERTBAR.innerText = '수식이 올바르지 않습니다.';
             return true;
         }
+
         // 숫자와 연산자로 이루어지지 않은 경우
-        const numberRex = /[^0-9]/g;
-        const operatorRex = /[^\*\/()+-\.]/;
-        const opArr = value.match(numberRex);
+        const notNumberRex = /[^0-9]/g; //숫자가 아닌 문자 정규식
+        const notOperatorRex = /[^\*\/()+-\.]/g; // 연산자가 아닌 문자 정규식
+        const opArr = value.match(notNumberRex);
         // if (koreanRex.test(value) || englishRex.test(value)) {
-        if (operatorRex.test(opArr.join(''))) {
+        if (notOperatorRex.test(opArr.join(''))) {
+            //숫자를 뺀 문자들 중에서 연산자가 아닌 문자가 있는 지를 체크.
             ALERTBAR.style.display = 'flex';
             ALERTBAR.innerText = '숫자와 연산자가 아닌 글자는 입력할 수 없습니다.';
             return true;
@@ -79,11 +82,11 @@
     }
 
     function calculate() {
-        if (INPUT_VALUE.value === '') {
+        if (INPUT_VALUE.value === '' || INPUT_VALUE.value === '0') {
             return;
         }
         if (checkValid(INPUT_VALUE.value)) {
-            INPUT_VALUE.value = 0;
+            INPUT_VALUE.value = '0';
             EXPRESSION.innerText = '';
             deleteAlarm();
             return;
